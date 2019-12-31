60fbffad5bcf406784a4bbdde321fb398191aad4
Simone Taeggi <startae14@gmail.com>
2019-12-31 10:24:33 +0100

    configure babelrc to use the ES modules just in test env

--- .babelrc.js
+++ .babelrc.js
@@ -1,8 +1,10 @@
 const isProd = String(process.env.NODE_ENV) === 'production'
+// check the test Env
+const isTest = String(process.env.NODE_ENV) === 'test'
 
 module.exports = {
   presets: [
-    ['@babel/preset-env', {modules: false}],
+    ['@babel/preset-env', { modules: isTest ? 'commonjs' : false }],
     '@babel/preset-react',
     [
       '@emotion/babel-preset-css-prop',
