eb82064960f71844d827a4a1130f0d362d61848b
Simone Taeggi <startae14@gmail.com>
2020-01-02 17:55:31 +0100

    add utilities files in the test as node modules

--- src/shared/__tests__/calculator-display.js
+++ src/shared/__tests__/calculator-display.js
@@ -1,33 +1,31 @@
 import React from 'react'
-import {render} from '@testing-library/react'
+import {render} from 'calculator-test-utils'
+import {light} from '../../themes'
 import CalculatorDisplay from '../calculator-display'
 
 test('renders', () => {
-  const {container} = render(<CalculatorDisplay value="0" />)
-  expect(container).toMatchInlineSnapshot(`
+  const {container} = render(<CalculatorDisplay value="0" />, {theme: light})
+  expect(container.firstChild).toMatchInlineSnapshot(`
     .emotion-0 {
       position: relative;
-      color: white;
-      background: #1c191c;
       line-height: 130px;
       font-size: 6em;
       -webkit-flex: 1;
       -ms-flex: 1;
       flex: 1;
+      color: #1c191c;
+      background: white;
     }
 
-    <div>
+    <div
+      class="emotion-0 emotion-1"
+    >
       <div
-        class="emotion-0"
-        id="calculator-display"
+        class="autoScalingText"
+        data-testid="total"
+        style="transform: scale(1,1);"
       >
-        <div
-          class="autoScalingText"
-          data-testid="total"
-          style="transform: scale(1,1);"
-        >
-          0
-        </div>
+        0
       </div>
     </div>
   `)
